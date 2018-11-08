6abb5fa85bfd37bdc40257626abc9c11223ce989
Jhonnatan Urueña <jhonnatan.uruea@SINCO.NET>
2018-11-08 12:23:35 -0500

    :valiaciones

--- virtual-scroll.js
+++ virtual-scroll.js
@@ -23,7 +23,6 @@ var VirtualScroll = /** @class */ (function () {
         this._v_object = new VirtualObject();
         this._init();
     }
-    // mentiras que no
     VirtualScroll.prototype._init = function () {
         var _this = this;
         this._v_object._from = Math.floor(this._length_viewer()) + 20;
@@ -37,8 +36,8 @@ var VirtualScroll = /** @class */ (function () {
         console.log("total renderizado:____" + this._v_object._from);
         var _visor_scrollind = document.getElementById(this._setting.id_visor);
         if (_visor_scrollind != null) {
-            _visor_scrollind.addEventListener('scroll', function (e) {
-                if (_visor_scrollind != null && _visor_scrollind.scrollTop != 0) {
+            _visor_scrollind.addEventListener("scroll", function (e) {
+                if (_visor_scrollind != null) {
                     _this._scrolling(_visor_scrollind.scrollTop);
                 }
             });
@@ -47,15 +46,16 @@ var VirtualScroll = /** @class */ (function () {
     VirtualScroll.prototype._length_viewer = function () {
         var _length = document.getElementById(this._setting.id_visor);
         if (_length != null) {
-            return (_length.offsetHeight / this._setting._length_tr);
+            return _length.offsetHeight / this._setting._length_tr;
         }
         else {
             return 0;
         }
     };
     VirtualScroll.prototype._tr_scroll = function (id, _length) {
-        var _html_tr = '';
-        _html_tr = '<tr><td id=' + id + ' style="height:' + _length + 'px"></td></tr>';
+        var _html_tr = "";
+        _html_tr =
+            "<tr><td id=" + id + ' style="height:' + _length + 'px"></td></tr>';
         return _html_tr;
     };
     VirtualScroll.prototype._chunked = function (_i, _f) {
@@ -73,7 +73,7 @@ var VirtualScroll = /** @class */ (function () {
     };
     VirtualScroll.prototype._get_top = function () {
         var result = Math.floor(this._v_object._scroll / this._setting._length_tr);
-        if ((result + this._get_length_viewver()) > this._v_object._length_Datos) {
+        if (result + this._get_length_viewver() > this._v_object._length_Datos) {
             result = this._v_object._length_Datos - this._get_length_viewver();
         }
         return result;
@@ -89,15 +89,16 @@ var VirtualScroll = /** @class */ (function () {
             this._v_object._container = _container;
             console.log("contenedor:__________" + this._v_object._container);
             var _from = this._v_object._from * this._v_object._container;
-            var _inital = this._v_object._from * (this._v_object._container - 1) - this._get_length_viewver();
+            var _inital = this._v_object._from * (this._v_object._container - 1) -
+                this._get_length_viewver();
             var _missing = this._v_object._length_Datos - _from;
-            if (_inital < 0) {
+            if (_inital < 0 || _scrolled == 0) {
                 _inital = 0;
             }
             if (_from > this._v_object._length_Datos) {
                 _from = this._v_object._length_Datos;
             }
-            this._v_object._html = '';
+            this._v_object._html = "";
             this._v_object._html += this._tr_scroll(tr_primero, _inital * this._setting._length_tr);
             this._v_object._html += this._setting.fn_chunked(_inital, _from, this._setting.source);
             this._v_object._html += this._tr_scroll(tr_ultimo, _missing * this._setting._length_tr);
@@ -111,8 +112,11 @@ var VirtualScroll = /** @class */ (function () {
         var _register = this._get_top() + this._get_length_viewver();
         var result = this._container(_register);
         //decimal
-        if (result.toString().split('.').length == 2) {
-            var _decimal = parseFloat(result.toString().split('.')[1].substr(0, 1));
+        if (result.toString().split(".").length == 2) {
+            var _decimal = parseFloat(result
+                .toString()
+                .split(".")[1]
+                .substr(0, 1));
             if (_decimal > 0) {
                 result += 1;
             }
